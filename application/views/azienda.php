809b8a2b8c9682a70321617d0c52552dd85dbea1
Alessandro Nocella <alessandro.nocella@phone4biz.it>
2016-05-11 12:29:19 +0200

    sistemato layout azienda

--- application/views/azienda.php
+++ application/views/azienda.php
@@ -18,7 +18,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                <div class="col-sm-8 col-md-8">
                     <p>Vivamus vestibulum neque quis nunc convallis venenatis. Nulla tristique lorem sit amet ipsum ornare sit amet feugiat nulla condimentum. Sed faucibus volutpat nunc, at ullamcorper augue elementum id. Quisque at lectus leo, nec placerat mi. Curabitur egestas eleifend interdum. Suspendisse potenti. Suspendisse nec risus fermentum. </p>
                     <p class="columns_2">In hac habitasse platea dictumst. In hac habitasse platea dictumst. Donec aliquet tellus enim, a tincidunt nulla. Praesent mollis felis at nulla fermentum mattis. Vivamus vestibulum neque quis nunc convallis venenatis. Nulla tristique lorem sit amet ipsum ornare sit amet feugiat nulla condimentum. Sed faucibus volutpat nunc, at ullamcorper augue elementum id. Quisque at lectus leo, nec placerat mi. Curabitur egestas eleifend interdum. Suspendisse potenti. Suspendisse nec risus fermentum sapien congue fermentum sed at lorem. Suspendisse quis sem lorem, a euismod nisl. Nullam eget tortor purus, id molestie sapien. Curabitur egestas eleifend interdum. Curabitur egestas eleifend interdum.</p>
-                    <p><img src="images/clients/jquery.jpg" width="140" height="70" alt="jquery"></p>
+                    <p><img src="<?php echo base_url('images/partner-2.jpg'); ?>" width="140" height="70" alt="jquery"></p>
                     <blockquote>
                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                          <small>Clients are talking about us</small> </blockquote>
@@ -26,7 +26,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                <div class="col-sm-4 col-md-4">
                     <div class="service_teaser vertical">
                          <div class="service_photo">
-                              <figure style="background-image:url(images/serv_6.jpg)"></figure>
+                              <figure style="background-image:url(<?php echo base_url('images/serv_6.jpg'); ?>)"></figure>
                          </div>
                          <div class="service_details">
                               <h2 class="section_header skincolored"><b>Teaser</b>Element<small>has many styles!</small></h2>
@@ -35,57 +35,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                </div>
           </div>
      </div>     
-     </section>
-     <section class="call_to_action"> <img alt="responsive" src="images/star.png">
-          <div class="container">
-               <h3>give your creative ideas a boost</h3>
-               <h4>mouse over our team photos to see a cool effect!</h4>
-          </div>     
-     </section>
-     <section class="team_members">
-          <div class="container">
-               <h2 class="section_header fancy centered">Meet our Team... <small>they are always up to something good!</small></h2>
-               <div class="row">
-                    <div class="col-sm-6 col-md-3">
-                         <div class="team_member">
-                              <figure style="background-image: url(images/1b.jpg)"><img src="images/1a.jpg" alt="1a"></figure>
-                              <h5>Johny Boycot</h5>
-                              <small>Web-Developer / Founder</small>
-                              <hr>
-                              <div class="team_social"> <a href="https://www.facebook.com"><i class="fa fa-facebook"></i></a> <a href="https://twitter.com/leonartgr"><i class="fa fa-twitter"></i></a> <a href="#pinterest"><i class="fa fa-linkedin"></i></a> <a href="https://github.com/PlethoraThemes"><i class="fa fa-github-alt"></i></a> </div>
-                              <p class="short_bio">In hac habitasse platea dictumst. Suspendisse non tellus ligula. Morbi molestie feugiat tortor a hendrerit.</p>
-                         </div>
-                    </div>
-                    <div class="col-sm-6 col-md-3">
-                         <div class="team_member">
-                              <figure style="background-image: url(images/2b.jpg)"><img src="images/2a.jpg" alt="2a"></figure>
-                              <h5>Lisa Starborn</h5>
-                              <small>Designer / Founder</small>
-                              <hr>
-                              <div class="team_social"> <a href="https://www.facebook.com"><i class="fa fa-facebook"></i></a> <a href="https://twitter.com/leonartgr"><i class="fa fa-twitter"></i></a> <a href="#pinterest"><i class="fa fa-linkedin"></i></a> <a href="https://github.com/PlethoraThemes"><i class="fa fa-github-alt"></i></a> </div>
-                              <p class="short_bio">In hac habitasse platea dictumst. Suspendisse non tellus ligula. Morbi molestie feugiat tortor a hendrerit.</p>
-                         </div>
-                    </div>
-                    <div class="col-sm-6 col-md-3">
-                         <div class="team_member">
-                              <figure style="background-image: url(images/3b.jpg)"><img src="images/3a.jpg" alt="3a"></figure>
-                              <h5>Ben Folklore</h5>
-                              <small>Photographer / Model</small>
-                              <hr>
-                              <div class="team_social"> <a href="https://www.facebook.com"><i class="fa fa-facebook"></i></a> <a href="https://twitter.com/leonartgr"><i class="fa fa-twitter"></i></a> <a href="#pinterest"><i class="fa fa-linkedin"></i></a> <a href="https://github.com/PlethoraThemes"><i class="fa fa-github-alt"></i></a> </div>
-                              <p class="short_bio">In hac habitasse platea dictumst. Suspendisse non tellus ligula. Morbi molestie feugiat tortor a hendrerit.</p>
-                         </div>
-                    </div>
-                    <div class="col-sm-6 col-md-3">
-                         <div class="team_member">
-                              <figure style="background-image: url(images/4b.jpg)"><img src="images/4a.jpg" alt="4a"></figure>
-                              <h5>Danny Stronghold</h5>
-                              <small>Handsome / Driver</small>
-                              <hr>
-                              <div class="team_social"> <a href="https://www.facebook.com"><i class="fa fa-facebook"></i></a> <a href="https://twitter.com/leonartgr"><i class="fa fa-twitter"></i></a> <a href="#pinterest"><i class="fa fa-linkedin"></i></a> <a href="#googleplus"><i class="fa fa-google-plus"></i></a> </div>
-                              <p class="short_bio">In hac habitasse platea dictumst. Suspendisse non tellus ligula. Morbi molestie feugiat tortor a hendrerit.</p>
-                         </div>
-                    </div>
-               </div>
-          </div>     
      </section>
